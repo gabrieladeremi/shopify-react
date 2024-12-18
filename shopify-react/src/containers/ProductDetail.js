93826b350d626c4ff0442a8fe272abc3313b8a80
aderemi <73750355+gabrieladeremi@users.noreply.github.com>
2024-12-18 15:05:26 +0000

    remove selected product on back press

--- shopify-react/src/containers/ProductDetail.js
+++ shopify-react/src/containers/ProductDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { selectedProduct } from "../redux/actions/productActions";
+import { selectedProduct, removeSelectedProduct } from "../redux/actions/productActions";
 
 const ProductDetail = () => {
   const product = useSelector((state) => state.product)
@@ -22,6 +22,9 @@ const ProductDetail = () => {
 
   useEffect(() => {
     if (id && id !== "") fetchProductDetail()
+    return () => {
+      dispatch(removeSelectedProduct())
+    }
   }, [id])
 
   return (
